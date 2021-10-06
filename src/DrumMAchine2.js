d3e115baa1f90f1f4e1af094131b46a72aa90159
JeanM-Pro <jeancenteno54@gmail.com>
2021-10-06 12:36:18 -0500

    first commit

---
+++ src/DrumMAchine2.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { samples } from './data'
+
+export const DrumMAchine2 = () => {
+
+
+
+    return (
+        <div className='content'>
+
+            <div id='drum-machine'>
+                
+                <div className='pad-content'>
+
+
+
+                </div>
+
+                <div className='display-content'>
+
+                    <div id='display'>
+                        
+                    </div>
+
+                </div>
+            
+            </div>
+        </div>
+    )
+}
